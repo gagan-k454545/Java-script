bfeb8832166374676b47dd4bcce633f6659d2a36
gagan-k454545 <sincostan3030@gmail.com>
2024-06-23 18:29:05 +0530

    js update

--- javascript 1/script.js
+++ javascript 1/script.js
@@ -1,3 +1,11 @@
 alert("hellow java script")
 console.log("code is running ")
 console.log(3+3)
+
+
+var a = prompt("enter your no ")
+var b=prompt("enter your no ")
+c=parseInt(a)
+d=parseInt(b)
+
+alert(c+d)
