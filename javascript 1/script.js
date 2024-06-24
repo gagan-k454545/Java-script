f76406b1e1e21b986de772f62995e50037feb775
gagan-k454545 <sincostan3030@gmail.com>
2024-06-24 22:35:44 +0530

    js update

--- javascript 1/script.js
+++ javascript 1/script.js
@@ -3,7 +3,7 @@ console.log("code is running ")
 console.log(3+3)
 
 
-var a = prompt("enter your no ")
+var a = prompt("enter your no ")//take input from the user in browser
 var b=prompt("enter your no ")
 c=parseInt(a)
 d=parseInt(b)
