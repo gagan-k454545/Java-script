9ef0b9d29fbd44dfa1c511ebe37da1e45df9c824
gagan-k454545 <sincostan3030@gmail.com>
2024-06-26 22:04:39 +0530

    js code is added

---
+++ javascript 3/script.js
@@ -0,0 +1,29 @@
+console.log("Hellow I am conditional statement")
+
+let age =17
+let grase=2
+console.log(age+grase)
+console.log(age-grase)
+console.log(age/grase)
+console.log(age**grase)
+console.log(age%grase)
+
+if(age+grase>18){
+    console.log("You can drive")
+}
+else{
+    console.log("You cannot drive")
+}
+
+
+let y=10
+let x=10
+console.log(y+=y)
+console.log(x=++x)
+
+let r=2
+console.log(r+=y)
+let o="4"
+let n=4
+console.log(o==n)
+console.log(o===n)
