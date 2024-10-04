e72aafcfcbaa4650cb19a2293f838f90f2b16932
gagan-k454545 <sincostan3030@gmail.com>
2024-10-04 21:08:07 +0530

    code added funciton

---
+++ javascript 5/script.js
@@ -0,0 +1,27 @@
+console.log("This is the tutorial on function ");
+function name(name)
+{
+    console.log("Hellow how are you "+name);
+    console.log("Hellow how are you "+name);
+    console.log("Hellow how are you "+name);
+}
+
+
+name("gagan")
+
+
+//function is used for perfore  the repatative task
+console.log("//function is used for perfore  the repatative task");
+function sum(a,b){
+    return a+b;
+}
+let a=sum(34,54)
+let a1=sum(3,54)
+let a2=sum(4,54)
+let a3=sum(34,5)
+
+console.log(a);
+console.log(a2);
+console.log(a1);
+console.log(a1);
+console.log(a3);
